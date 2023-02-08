517661278fffe5df49404e1391c68a7824c41b86
manvithshetty507 <manvithshetty18@gmail.com>
2023-02-09 00:19:24 +0530

    5th commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,13 +8,14 @@ public class Main {
     }
 }
 class A{
-    public static  String meth(){
-
+    public String meth(){
         return "Invoking method from class A";
     }
 }
 class B extends A{
-    public static String meth(){
+    @Override
+    public String meth(){
         return "Method is overridden in Extendend class B";
     }
 }
+
